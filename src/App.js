f7c9dd19aeb754e7ab87e6497f7b277209c57b71
NiniGogidze <119741770+NiniGogidze@users.noreply.github.com>
2023-06-28 15:27:29 +0400

    created log in form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Registration from "./folder/Registration";
+import {useState} from "react";
+
 
 function App() {
+  const [emailAddress, setEmailAddress] = useState ("");
+  const [password, setPassword] = useState ("");
+
+  const handleSubmit = () => {
+    const data = {
+      emailAddress,
+      password
+    }
+    console.log(data) 
+
+    if (emailAddress.trim().length === 0 && password.trim().length === 0) {
+      return alert ('შეავსე ყველა ველი')
+    }
+
+    if (password.length < 8) {
+      return alert ('მაქსიმუმ რვა სიმბოლოა საჭირო')
+    }
+  }
+
+  console.log({emailAddress})
+  console.log({password})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Registration />
+      <input type="email" name="Email address" placeholder="Email address" onChange={(e) => setEmailAddress(e.target.value)} className="first-input"/>
+      <input type="password" name="Password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="second-input"/>
+      <button onClick={handleSubmit}>create accaunt</button>
     </div>
   );
 }
---
+++ src/folder/Registration.jsx
@@ -0,0 +1,21 @@
+import './style.css'
+
+function Registration (props){
+    return (
+        <div>
+            <div className='green-box'></div>
+            <div className='greenbox'></div>
+            <div className='whitebox'>
+                <div>
+                    <h1>create an accaunt</h1>
+                    <p>Create an account to enjoy all the services without any ads for free!</p>
+                </div>
+                <p className='text'>Already Have An Account? Sign In</p>
+            </div>
+        </div>
+    )
+    
+
+}
+
+export default Registration;
